e4c391b07a34fbecf31e373e47b38b9049d7ccef
lvleibing01 <lvleibing01@baidu.com>
2014-10-30 14:13:06 +0800

    rabbitmq_client.py：重写client相关channel的设计

--- packages/abstract/rabbitmq_client.py
+++ packages/abstract/rabbitmq_client.py
@@ -11,7 +11,7 @@ from abstract_module import *
 
 
 class RabbitMQClient(AbstractModule):
-    """abstract RabbitMQ client
+    """Abstract RabbitMQ client
     """
 
     def __init__(self):
@@ -49,38 +49,40 @@ class RabbitMQClient(AbstractModule):
         mq_host, mq_port = self.config['rmq_client']['host'], self.config['rmq_client']['port']
         self.mq_conn = pika.BlockingConnection(pika.ConnectionParameters(host=mq_host, port=int(mq_port)))
 
-        self.init_sub_client()
-        self.init_pub_client()
+        self.init_consumer_client()
+        self.init_producer_client()
 
-    def init_sub_client(self):
+    def init_consumer_client(self):
         """
         """
 
-        sub_exchange = self.config['rmq_client']['sub_exchange']
-        if not sub_exchange:
-            return False
+        exchange = self.config['rmq_client']['consumer_exchange']
+        exchange_type = exchange['type']
+        exchange_name = exchange['name']
+        routing_key = exchange['routing_key']
 
-        self.sub_channel = self.mq_conn.channel()
-        self.sub_channel.exchange_declare(exchange=sub_exchange, type='fanout')
+        self.consumer_channel = self.mq_conn.channel()
+        self.consumer_channel.exchange_declare(exchange=exchange_name, type=exchange_type)
 
-        result = self.sub_channel.queue_declare(exclusive=True)
+        result = self.consumer_channel.queue_declare(exclusive=True)
         queue_name = result.method.queue
-        self.sub_channel.queue_bind(exchange=sub_exchange, queue=queue_name)
+        self.consumer_channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key=routing_key)
 
-        self.sub_channel.basic_consume(self.callback, queue=queue_name, no_ack=True)
+        self.consumer_channel.basic_consume(self.callback, queue=queue_name, no_ack=True)
 
         return True
 
-    def init_pub_client(self):
+    def init_producer_client(self):
         """
         """
 
-        pub_exchange = self.config['rmq_client']['pub_exchange']
-        if not pub_exchange:
-            return False
+        exchange = self.config['rmq_client']['producer_exchange']
+        exchange_type = exchange['type']
+        exchange_name = exchange['name']
+        routing_key = exchange['routing_key']
 
-        self.pub_channel = self.mq_conn.channel()
-        self.pub_channel.exchange_declare(exchange=pub_exchange, type='fanout')
+        self.producer_channel = self.mq_conn.channel()
+        self.producer_channel.exchange_declare(exchange=exchange_name, type=exchange_type)
 
         return True
 
@@ -96,22 +98,15 @@ class RabbitMQClient(AbstractModule):
         """
         """
 
-        if not self.sub_channel:
-            return False
-
         self.sub_channel.start_consuming()
 
         return True
 
-    def publish(self, msg):
+    def publish(self, exchange=self.config['rmq_client']['pub_exchange'], routing_key='', body=''):
         """
         """
 
-        if not self.pub_channel:
-            return False
-
-        pub_exchange = self.config['rmq_client']['pub_exchange']
-        self.pub_channel.basic_publish(exchange=pub_exchange, routing_key='', body=msg)
+        self.pub_channel.basic_publish(exchange=exchange, routing_key=routing_key, body=body)
 
         return True
 
@@ -125,7 +120,7 @@ if __name__ == '__main__':
 
         msg = 'blowing in the wind. {0}'.format(i)
         print msg
-        rmq_client.publish(msg)
+        rmq_client.publish(body=msg)
 
         time.sleep(1)
 
