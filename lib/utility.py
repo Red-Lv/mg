11be0d8ed354450da9238d53c42e008b7b71c597
lvleibing01 <lvleibing01@baidu.com>
2014-11-01 18:26:13 +0800

    utility.py：新增公用函数

--- lib/utility.py
+++ lib/utility.py
@@ -6,6 +6,7 @@
 # desc:
 
 import json
+import requests
 
 
 def json_to_str(obj, obj_encoding='UTF-8', str_encoding='UTF-8'):
@@ -14,3 +15,44 @@ def json_to_str(obj, obj_encoding='UTF-8', str_encoding='UTF-8'):
         return json.dumps(obj, encoding=obj_encoding, ensure_ascii=False).encode(str_encoding)
     except Exception as e:
         return ''
+
+
+def read_file_content_iter(file_path=''):
+    """
+    """
+
+    try:
+        with open(file_path) as fp:
+            for line in fp:
+                line = line.strip()
+                if not line:
+                    continue
+                yield line
+    except Exception as e:
+        pass
+
+
+def read_file_content(file_path=''):
+    """
+    """
+
+    try:
+        with open(file_path) as fp:
+            return fp.read()
+    except Exception as e:
+        return ''
+
+
+def read_web_content(url=''):
+    """
+    """
+
+    try:
+        r = requests.get(url)
+        if r.status_code != requests.codes.ok:
+            return ''
+        return r.text.encode('UTF-8')
+    except Exception as e:
+        return ''
+
+    return ''
