7ce24b2da64e58f0741c7dc3c6709226592c0ec8
lvleibing01 <lvleibing01@baidu.com>
2014-11-01 19:39:42 +0800

    修复相关代码

--- lib/utility.py
+++ lib/utility.py
@@ -5,6 +5,7 @@
 # author: lvleibing01
 # desc:
 
+import codecs
 import json
 import requests
 
@@ -17,12 +18,12 @@ def json_to_str(obj, obj_encoding='UTF-8', str_encoding='UTF-8'):
         return ''
 
 
-def read_file_content_iter(file_path=''):
+def read_file_content_iter(file_path='', encoding='UTF-8'):
     """
     """
 
     try:
-        with open(file_path) as fp:
+        with codecs.open(file_path, encoding=encoding) as fp:
             for line in fp:
                 line = line.strip()
                 if not line:
@@ -32,27 +33,29 @@ def read_file_content_iter(file_path=''):
         pass
 
 
-def read_file_content(file_path=''):
+def read_file_content(file_path='', encoding='UTF-8'):
     """
     """
 
     try:
-        with open(file_path) as fp:
+        with codecs.open(file_path, encoding=encoding) as fp:
             return fp.read()
     except Exception as e:
         return ''
 
 
-def read_web_content(url=''):
+def read_web_content(url='', encoding=None):
     """
     """
 
     try:
         r = requests.get(url)
+        if encoding:
+            r.encoding = encoding
         if r.status_code != requests.codes.ok:
-            return ''
-        return r.text.encode('UTF-8')
+            return u''
+        return r.text
     except Exception as e:
-        return ''
+        return u''
 
-    return ''
+    return u''
