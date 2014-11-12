ecefd401ff11d381964d86b162e2357ef483f3b8
Lv Leibing <lvleibing01@baidu.com>
2014-11-12 12:23:26 +0800

    update

--- packages/concrete/message/msg_producer.py
+++ packages/concrete/message/msg_producer.py
@@ -8,6 +8,7 @@
 import json
 import time
 
+from lib.globals import *
 from packages.abstract.abstract_module import *
 from packages.abstract.rabbitmq_client import *
 
@@ -25,6 +26,7 @@ class MsgProducer(RabbitMQClient):
         RabbitMQClient.init(self, config_path)
 
         self.mysql_db = frame.mysql_db
+        self.mongo_db = frame.mongo_db
 
         return True
 
@@ -40,11 +42,16 @@ class MsgProducer(RabbitMQClient):
 
     def publish(self):
 
-        dir_info_list = self.get_dir_info_list()
-        for dir_info in dir_info_list:
-            msg = json.dumps(dir_info, encoding='GBK', ensure_ascii=False)
+        entity_list = self.get_entity_list()
+        for i in xrange(10000000):
+            entity = entity_list[i % len(entity_list)]
+            entity['last_content_index'] = int(time.time() * 1000)
+            msg = json.dumps(entity, encoding='GBK', ensure_ascii=False)
+            print msg.encode('UTF-8')
             RabbitMQClient.publish(self, body=msg.encode('UTF-8'))
-            time.sleep(0.1)
+            RabbitMQClient.publish(self, body=msg.encode('UTF-8'))
+            time.sleep(0.001)
+            LOG_INFO('msg published: %s', msg.encode('UTF-8'))
 
         return True
 
@@ -65,9 +72,29 @@ class MsgProducer(RabbitMQClient):
 
         return dir_info_list
 
+    def get_entity_list(self):
+
+        entity_list = []
+
+        db = self.mongo_db.fetch_dbhandler('db_entity_agg')
+        if not db:
+            return entity_list 
+
+        collection = db['entity_agg_1']
+        cursor = collection.find()
+        for document in cursor:
+            del(document['_id'])
+            entity_list.append(document)
+
+        cursor.close()
+        self.mongo_db.push_back_dbhandler(db)
+
+        return entity_list
+
+
 if __name__ == '__main__':
 
-    msg_producer = MsgProduer()
+    msg_producer = MsgProducer()
     msg_producer.init('./conf/msg_producer.conf')
 
     msg_producer.publish()
